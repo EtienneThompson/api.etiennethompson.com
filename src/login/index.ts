d5860f39d504b4e40ed61d747c858eb25a430ba6
Etienne Thompson <et@etiennethompson.com>
2021-08-14 19:25:40 -0700

    Move most query logic to database file.

--- src/login/index.ts
+++ src/login/index.ts
@@ -1,46 +1,34 @@
 import e, { Request, Response } from "express";
 import { LoginRequest, UserEntry, ApplicationEntry } from "./types";
-import { connectToDatabase } from "../utils/database";
+import { connectToDatabase, performQuery } from "../utils/database";
 
 export const loginHandler = async (req: Request, res: Response) => {
   var requestBody = req.body as LoginRequest;
 
   const client = connectToDatabase();
 
-  const user = await client.query(
-    `SELECT * FROM users WHERE username='${requestBody.username}' AND password='${requestBody.hashedPassword}'`
-  );
   let clientId: string = "";
-  if (user) {
-    if (user.rows.length === 0) {
-      client.end();
-      res.status(404);
-      res.send({ message: "That user doesn't exist." });
-      return;
-    } else {
-      const entry = user.rows[0] as UserEntry;
-      clientId = entry.clientid;
-    }
+  const userQuery = `SELECT * FROM users WHERE username='${requestBody.username}' AND password='${requestBody.hashedPassword}'`;
+  const userRows = await performQuery(client, userQuery);
+  if (userRows) {
+    const entry = userRows[0] as UserEntry;
+    clientId = entry.clientid;
   } else {
     client.end();
+    res.status(404);
+    res.send({ message: "That user doesn't exist/" });
   }
 
-  const application = await client.query(
-    `SELECT * FROM applications WHERE applicationid='${requestBody.appid}'`
-  );
   let redirectUrl: string = "";
-  if (application) {
-    if (application.rows.length === 0) {
-      client.end();
-      res.status(404);
-      res.send({ message: "That application doesn't exist." });
-      return;
-    } else {
-      const appEntry = application.rows[0] as ApplicationEntry;
-      redirectUrl = appEntry.redirecturl;
-    }
+  const applicationQuery = `SELECT * FROM applications WHERE applicationid='${requestBody.appid}'`;
+  const applicationRows = await performQuery(client, applicationQuery);
+  if (applicationRows) {
+    const entry = applicationRows[0] as ApplicationEntry;
+    redirectUrl = entry.redirecturl;
   } else {
     client.end();
+    res.status(404);
+    res.send({ message: "That application doesn't exist." });
   }
 
   client.end();
--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,5 +1,9 @@
 import { Client } from "pg";
 
+/**
+ * Connect to the database and return a client object for further interactions.
+ * @returns Client object for interacting with the database.
+ */
 export const connectToDatabase = (): Client => {
   const client = new Client({
     connectionString: process.env.DATABASE_URL,
@@ -11,4 +15,31 @@ export const connectToDatabase = (): Client => {
   client.connect();
 
   return client;
+};
+
+export const performQuery = async (client: Client, query: string) => {
+  return await makeSingleQuery(client, query);
+};
+
+/**
+ * Makes a single query and returns the results if there were any.
+ * @param client the database to perform the query on.
+ * @param query the query to perform.
+ * @returns The results of the query, undefined if no results, and null if no response.
+ */
+const makeSingleQuery = async (
+  client: Client,
+  query: string
+): Promise<any[] | undefined | null> => {
+  const response = await client.query(query);
+
+  if (response) {
+    if (response.rows.length === 0) {
+      return undefined;
+    } else {
+      return response.rows;
+    }
+  } else {
+    return null;
+  }
 };
