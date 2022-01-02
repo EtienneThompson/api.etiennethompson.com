951b7431e24086f1f0e40a574e4db3afaf077b62
Etienne Thompson <et@etiennethompson.com>
2022-01-02 16:27:54 -0700

    Return parent_folder for item and folder.

--- src/inventory/index.ts
+++ src/inventory/index.ts
@@ -7,7 +7,7 @@ export const getFolder = async (req: Request, res: Response) => {
   let { code, rows } = await performQuery(getUserIdQuery);
   // The client id was verified in middleware, so this should always return a value.
   let userid = rows[0].userid;
-  const getFolderQuery = `SELECT folderid, name, picture, description, created, modified FROM folders WHERE folderid='${params.folderid}' AND owner='${userid}'`;
+  const getFolderQuery = `SELECT folderid, name, picture, description, parent_folder, created, modified FROM folders WHERE folderid='${params.folderid}' AND owner='${userid}'`;
   ({ code, rows } = await performQuery(getFolderQuery));
   if (code !== 200) {
     res.status(404);
@@ -23,6 +23,7 @@ export const getFolder = async (req: Request, res: Response) => {
   if (code === 200) {
     let folderChildren = rows.map((child) => {
       child.type = "folder";
+      child.id = child.folderid;
       return child;
     });
     children = children.concat(folderChildren);
@@ -31,6 +32,7 @@ export const getFolder = async (req: Request, res: Response) => {
   if (code === 200) {
     let itemChildren = rows.map((child) => {
       child.type = "item";
+      child.id = child.itemid;
       return child;
     });
     children = children.concat(itemChildren);
@@ -46,7 +48,7 @@ export const getItem = async (req: Request, res: Response) => {
   let { code, rows } = await performQuery(getUserIdQuery);
   // The client id was verified in middleware, so this should always return a value.
   let userid = rows[0].userid;
-  const getItemQuery = `SELECT itemid, name, picture, description, created, updated FROM items WHERE itemid='${params.itemid}' AND owner='${userid}'`;
+  const getItemQuery = `SELECT itemid, name, picture, description, parent_folder, created, updated FROM items WHERE itemid='${params.itemid}' AND owner='${userid}'`;
   ({ code, rows } = await performQuery(getItemQuery));
   if (code !== 200) {
     res.status(404);
