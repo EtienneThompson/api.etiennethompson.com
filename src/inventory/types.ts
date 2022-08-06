a60c83ae7e60446714758ca310f0170fafbcbf9f
Etienne Thompson <et@etiennethompson.com>
2022-08-05 17:35:45 -0700

    Comment and update inventory endpoints.

--- src/inventory/types.ts
+++ src/inventory/types.ts
@@ -1,3 +1,35 @@
+export interface ElementBase {
+  name?: string;
+  description?: string;
+  owner?: string;
+  picture?: string;
+  parent_folder?: string;
+  created?: string;
+  updated?: string;
+}
+
+export interface FolderChildren {
+  id: string | undefined;
+  folderid?: string;
+  itemid?: string;
+  type: "folder" | "item";
+  name: string;
+  picture: string;
+}
+
+export interface FolderElement extends ElementBase {
+  folderid?: string;
+  children?: FolderChildren[];
+}
+
+export interface ItemElement extends ElementBase {
+  itemid?: string;
+}
+
+export interface ElementGeneric extends ElementBase {
+  id?: string;
+}
+
 export interface CreateRequest {
   name: string;
   description: string;
@@ -21,4 +53,9 @@ export interface BreadcrumbItem {
   itemid: string;
   name: string;
   parent_folder: string;
+}
+
+export enum ElementTypes {
+  Item = "item",
+  Folder = "folder",
 }
