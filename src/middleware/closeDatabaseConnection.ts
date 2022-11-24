df0fc6e5155f4be09355ccd2c1ffe270a6b4e71d
Etienne Thompson <43585499+EtienneThompson@users.noreply.github.com>
2022-11-24 14:07:16 -0700

    Close aws connection after each request. (#42)

--- src/middleware/closeDatabaseConnection.ts
+++ src/middleware/closeDatabaseConnection.ts
@@ -1,11 +1,18 @@
 import { Request, Response } from "express";
+import { Client } from "pg";
 
 export const closeDatabaseConnection = async (
   req: Request,
   res: Response,
   next: any
 ) => {
-  const client = req.body.client;
+  const client: Client = req.body.client;
+
+  if (req.body.awsClient) {
+    const awsClient: Client = req.body.awsClient;
+    await awsClient.end();
+  }
+
   await client.end();
   res.end();
 };
