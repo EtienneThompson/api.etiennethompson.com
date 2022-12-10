38a90453d2595dacec7c849224d09e402bc2ff18
Etienne Thompson <et@etiennethompson.com>
2022-12-10 11:07:31 -0800

    Create single SQL query to get all clients.

--- src/thompsonaccounting/helpers.ts
+++ src/thompsonaccounting/helpers.ts
@@ -1,6 +1,37 @@
 import { Client } from "pg";
 import { performQuery, QueryProps } from "../utils/database";
-import { ColumnSchemaInfo } from "./types";
+import {
+  ClientDetails,
+  ClientDetailsTab,
+  ColumnType,
+  ColumnSchemaInfo,
+  DatabaseColumn,
+  IsNullable,
+} from "./types";
+import { capitalize } from "../utils/string";
+
+export const capitalizeName = (name: string): string => {
+  let pieces = name.split("_");
+  let ret_string: string = "";
+  for (let piece of pieces) {
+    piece = capitalize(piece);
+    ret_string += piece + " ";
+  }
+
+  ret_string = ret_string.substring(0, ret_string.length - 1);
+  return ret_string;
+};
+
+export const createColumnName = (name: string): string => {
+  let pieces = name.trim().split(" ");
+  let ret_string: string = "";
+  for (let piece of pieces) {
+    ret_string += piece.toLowerCase() + "_";
+  }
+
+  ret_string = ret_string.substring(0, ret_string.length - 1);
+  return ret_string;
+};
 
 export const getTableSchema = async (
   client: Client,
@@ -39,4 +70,97 @@ export const getEnumTypeValues = async (
   enumValues.splice(0, 0, "---");
 
   return enumValues;
+};
+
+export const getClientSchema = async (
+  client: Client
+): Promise<ClientDetails | undefined> => {
+  // Get all column names other than the id field.
+  let tableNames = await getTableSchema(client, "clients");
+  try {
+    tableNames = tableNames.slice(1);
+  } catch (e: any) {
+    return undefined;
+  }
+
+  const clientDetailsSchema: ClientDetailsTab[] = [];
+
+  // Get the schema for each table related to clients.
+  for (let tableName of tableNames) {
+    let rows;
+    try {
+      rows = await getTableSchema(client, tableName.column_name);
+    } catch (e: any) {
+      return undefined;
+    }
+
+    let fields: DatabaseColumn[] = [];
+
+    // Convert the database schema to the UI schema.
+    for (let field of rows) {
+      // Skip any id fields.
+      if (field.column_name.endsWith("id")) {
+        continue;
+      }
+
+      // Get type and default value based on data type.
+      let type: ColumnType;
+      let value: any;
+      let options: string[] | undefined;
+      switch (field.data_type) {
+        case "character varying":
+          type = "text";
+          value = "";
+          break;
+        case "boolean":
+          type = "checkbox";
+          value = false;
+          break;
+        case "text":
+          type = "textarea";
+          value = "";
+          break;
+        case "USER-DEFINED":
+          type = "select";
+          value = "---";
+
+          // Get the possible values for the user defined enum.
+          try {
+            options = await getEnumTypeValues(client, field.udt_name);
+          } catch (e: any) {
+            return undefined;
+          }
+
+          break;
+        default:
+          type = "text";
+          value = "";
+          break;
+      }
+
+      let fieldSchema: DatabaseColumn = {
+        name: field.column_name,
+        label: capitalizeName(field.column_name),
+        required: field.is_nullable === IsNullable.No,
+        type: type,
+        value: value,
+        options: options,
+      };
+
+      fields.push(fieldSchema);
+    }
+
+    clientDetailsSchema.push({
+      name: tableName.column_name,
+      label: capitalizeName(tableName.column_name),
+      fields: fields,
+    });
+  }
+
+  let clientDetails: ClientDetails = {
+    id: "",
+    tabs: clientDetailsSchema,
+  };
+
+  return clientDetails;
 };
