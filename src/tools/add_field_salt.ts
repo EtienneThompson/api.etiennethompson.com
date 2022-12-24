7833dba18bd17f8e442b75e79f786632f9bc094c
Etienne Thompson <43585499+EtienneThompson@users.noreply.github.com>
2022-12-24 14:52:18 -0700

    Create tool to add unique salt to field names. (#57)
    * Create tool to add unique salt to field names.
    
    * Remove debug console logs.
    
    * Update typescript compile target.

---
+++ src/tools/add_field_salt.ts
@@ -0,0 +1,66 @@
+import {
+  connectToAWSDatabase,
+  performFormattedQuery,
+} from "../utils/database";
+import {
+  createFieldName,
+  getClientSchema,
+} from "../thompsonaccounting/helpers";
+
+require("dotenv").config({ path: `./.env.local` });
+
+const rename_columns = async () => {
+  if (
+    !process.env.THOMPSON_ACCOUNTING_DATABASE_HOST ||
+    !process.env.THOMPSON_ACCOUNTING_DATABASE_USER ||
+    !process.env.THOMPSON_ACCOUNTING_DATABASE_PASSWORD ||
+    !process.env.THOMPSON_ACCOUNTING_DATABASE_DATABASE
+  ) {
+    return;
+  }
+
+  const client = await connectToAWSDatabase(
+    process.env.THOMPSON_ACCOUNTING_DATABASE_HOST,
+    process.env.THOMPSON_ACCOUNTING_DATABASE_USER,
+    process.env.THOMPSON_ACCOUNTING_DATABASE_PASSWORD,
+    process.env.THOMPSON_ACCOUNTING_DATABASE_DATABASE
+  );
+
+  const clientSchema = await getClientSchema(client);
+
+  if (!clientSchema) {
+    return;
+  }
+
+  for (let tab of clientSchema.tabs) {
+    for (let field of tab.fields) {
+      const newFieldName = createFieldName(tab.name, field.name);
+
+      let sql = `ALTER TABLE ${tab.name} RENAME COLUMN ${field.name} TO ${newFieldName};`;
+      console.log(sql);
+
+      let { code, rows } = await performFormattedQuery(client, sql);
+
+      if (code !== 200) {
+        console.log("ERROR!!!");
+      }
+
+      if (field.type === "select") {
+        let sql = `ALTER TYPE ${field.name} RENAME TO ${newFieldName};`;
+        console.log(sql);
+
+        let { code, rows } = await performFormattedQuery(client, sql);
+
+        if (code !== 200) {
+          console.log("ERROR!!!!");
+        }
+      }
+
+      console.log();
+    }
+  }
+
+  await client.end();
+};
+
+rename_columns();
